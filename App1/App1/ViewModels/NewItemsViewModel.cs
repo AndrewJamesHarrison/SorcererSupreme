f3d75ac2b4c647fc65c6b5a8eeae6ffd6cfd7de6
Andrew Harrison <aharrison92@gmail.com>
2017-05-13 16:48:39 +0800

    Initial Commit

---
+++ App1/App1/ViewModels/NewItemsViewModel.cs
@@ -0,0 +1,36 @@
+﻿using App1.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.ViewModels
+{
+    public class NewItemsViewModel : BaseViewModel
+    {
+        public List<Item> WordList => new List<Item>
+        {
+            new Item("Si", "Conditional/Interaction"),
+            new Item("Magnitudine", "Size"),
+            new Item("Morphosia", "Shape"),
+            new Item("Ex Nihil", "Create"),
+            new Item("Locatia", "Location"),
+            new Item("Emulus", "Imitate"),
+            new Item("Somateria", "Physical Object"),
+            new Item("Injectiv", "Insert"),
+            new Item("Extractus", "Extract"), //Extract - -no longer needed?
+            new Item("Crescere", "Increase"),
+            new Item("Contra", "Reverse"),
+            new Item("Transmutia", "Convert"),
+            new Item("Preventia", "Prevent"),
+            new Item("Vitalia", "Life"),
+            new Item("Vectora", "Kinetic Energy"),
+            new Item("Luminus", "Light Energy"),
+            new Item("Thermia", "Thermal Energy"),
+            new Item("Potentia", "Electrical Energy"),
+            new Item("Chronos", "Time"),
+            new Item("Arcano", "Magic"),
+        };
+
+        public Item SelectedItem { get; set; }
+    }
+}
---
+++ App1/App1/Views/NewItemPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using App1.Models;
+
+using Xamarin.Forms;
+using App1.ViewModels;
+
+namespace App1.Views
+{
+	public partial class NewItemPage : ContentPage
+	{
+
+        public NewItemsViewModel viewModel;
+
+		public NewItemPage()
+		{
+			InitializeComponent();
+
+			BindingContext = viewModel = new NewItemsViewModel(); ;
+		}
+
+		async void Save_Clicked(object sender, EventArgs e)
+		{
+			MessagingCenter.Send(this, "AddItem", viewModel.SelectedItem);
+			await Navigation.PopToRootAsync();
+		}
+	}
+}
