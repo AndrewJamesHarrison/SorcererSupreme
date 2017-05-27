0e7e062ca1c4ba1ca37aa0ef7aefe0c5e53d3cb2
Andrew Harrison <aharrison92@gmail.com>
2017-05-27 16:07:55 +0800

    Getting ready for beta release

--- App1/App1.Android/Properties/AssemblyInfo.cs
+++ App1/App1.Android/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using Android.App;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("App1.Android")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("WizardTricks.SorcererSupreme")]
+[assembly: AssemblyDescription("This app is a companion meant to help play the Pen and Paper game, The Sorcerer Supreme!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
@@ -18,7 +18,7 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
